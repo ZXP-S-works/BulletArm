fixed gripper rotation problem; add panda arm to open-close loop env

bulletarm/envs/close_loop_envs/open_close_loop_cluttered_grasping.py
@@ -170,7 +170,7 @@ if __name__ == '__main__':
   #                         [0.01, 0.25]])
   env_config = {'workspace': workspace, 'max_steps': 100, 'obs_size': 128, 'render': True, 'fast_mode': True,
                 'seed': 2, 'action_sequence': 'pxyzr', 'num_objects': 15, 'random_orientation': False,
-                'reward_type': 'step_left', 'simulate_grasp': True, 'perfect_grasp': False, 'robot': 'kuka',
+                'reward_type': 'step_left', 'simulate_grasp': True, 'perfect_grasp': False, 'robot': 'panda',
                 'object_init_space_check': 'point', 'physics_mode': 'fast', 'object_scale_range': (0.8, 0.8), 'hard_reset_freq': 1000}
   planner_config = {'random_orientation': False, 'dpos': 0.05, 'drot': np.pi/8}
   env_config['seed'] = 1

bulletarm/planners/close_loop_planner.py
@@ -14,7 +14,9 @@ class CloseLoopPlanner(BasePlanner):
     current_pos = self.env.robot._getEndEffectorPosition()
     current_rot = transformations.euler_from_quaternion(self.env.robot._getEndEffectorRotation())
     pos_diff = goal_pos - current_pos
-    rot_diff = np.array(goal_rot) % (np.pi / 2) - np.array(current_rot) % (np.pi / 2)  # assuming mirroring symmetric gripper
+    rot_diff = np.array(goal_rot) % np.pi - np.array(current_rot) % np.pi  # assuming mirroring symmetric gripper
+    rot_diff[rot_diff > np.pi / 2] -= np.pi
+    rot_diff[rot_diff < -np.pi / 2] += np.pi
 
     # R = np.array([[np.cos(-current_rot[-1]), -np.sin(-current_rot[-1])],
     #               [np.sin(-current_rot[-1]), np.cos(-current_rot[-1])]])